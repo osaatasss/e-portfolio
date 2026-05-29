6658f4b1d783440f2f3bf250f82022787d70b6f5
Apostol Robert <robert.apostol11@gmail.com>
2026-05-29 23:47:08 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <div className="flex h-screen items-center justify-center bg-slate-900">
+        <h1 className="text-4xl font-bold text-sky-400 underline">
+          Tailwind is working!
+        </h1>
+      </div>
+  )
+}
+export default App;
